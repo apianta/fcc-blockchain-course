901a18b03d0a986f204e716e7a7db683961d5443
Andrew Pianta <a.pianta92@gmail.com>
2022-02-09 14:16:11 -0500

    Fix formatting to comply w/ solidity... Add code.workspace to .gitignore

--- contracts/PriceFeed.sol
+++ contracts/PriceFeed.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsumerV3 {
-
     AggregatorV3Interface internal priceFeed;
 
     /**
@@ -12,19 +11,22 @@ contract PriceConsumerV3 {
      * Aggregator: ETH/USD
      * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
      */
+
     constructor() {
-        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        priceFeed = AggregatorV3Interface(
+            0x9326BFA02ADD2366b30bacB125260Af641031331
+        );
     }
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int) {
+    function getLatestPrice() public view returns (int256) {
         (
-            uint80 roundID, 
-            int price,
-            uint startedAt,
-            uint timeStamp,
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
         return price;
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.6.0;
 
 contract SimpleStorage {
-// external, public, internal, private
-// this will get initialized to 0!
+    // external, public, internal, private
+    // this will get initialized to 0!
     uint256 favoriteNumber;
     bool favoriteBool;
 
@@ -16,26 +16,20 @@ contract SimpleStorage {
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
-
     People public person = People({favoriteNumber: 2, Name: "Andrew"});
 
-
-    function store(uint256 _favoriteNumber) public{
+    function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
-        
     }
 
-    function retrieve() public view returns(uint256){
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
 
-// memory = only be stored during execution of the contract call
-// storage = data will persist even after execution
+    // memory = only be stored during execution of the contract call
+    // storage = data will persist even after execution
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
     }
-
-
-
 }
