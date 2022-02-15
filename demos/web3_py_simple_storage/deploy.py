ec6490d5b83c7c8a85ca7955e8f47288a7ff84ac
Andrew Pianta <a.pianta92@gmail.com>
2022-02-15 16:35:43 -0500

    finish build transaction... transaction signing

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -2,6 +2,9 @@ from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -35,7 +38,7 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # connecting to Ganache
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
 chain_id = 1337
-my_address = "0xB2E9526ECc7343260c9E2B5bdF5c7DAe1193C257"
+my_address = os.getenv("MY_ADDRESS")
 # make sure to always include the "0x" in the front
 private_key = os.getenv("PRIVATE_KEY")
 print(private_key)
@@ -51,8 +54,14 @@ print(nonce)
 # 2. sign the transaction
 # 3. send the transaction
 transaction = SimpleStorage.constructor().buildTransaction(
-    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
 )
 
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print(signed_txn)
 # environment variables
