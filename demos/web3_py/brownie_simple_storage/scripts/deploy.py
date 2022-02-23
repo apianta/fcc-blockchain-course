62fbceebd53c0011982f2fd7aaedb82598ec7b4d
Andrew Pianta <a.pianta92@gmail.com>
2022-02-23 14:30:11 -0500

    adds deploy.py brownie script

--- demos/web3_py/brownie_simple_storage/scripts/deploy.py
+++ demos/web3_py/brownie_simple_storage/scripts/deploy.py
@@ -1,13 +1,18 @@
 from ast import Import
 from brownie import accounts, config, SimpleStorage
 
+
 def deploy_simple_storage():
     account = accounts[0]
     simple_storage = SimpleStorage.deploy({"from": account})
-    print(simple_storage)
-    # print(account)
-    # account = accounts.add(config["wallets"]["from_key"])
-    # print(account)
+    # print(simple_storage)
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+
 
 def main():
     deploy_simple_storage()
