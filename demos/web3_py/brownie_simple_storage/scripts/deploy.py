af76cc6b4a284a1de6489c07e8558390dcb2c8f3
Andrew Pianta <a.pianta92@gmail.com>
2022-02-28 11:56:21 -0500

    updates deploy.py, adds read_value.py

--- demos/web3_py/brownie_simple_storage/scripts/deploy.py
+++ demos/web3_py/brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,9 @@
-from brownie import accounts, config, SimpleStorage
+from webbrowser import get
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = getAccount()
     simple_storage = SimpleStorage.deploy({"from": account})
     # print(simple_storage)
     stored_value = simple_storage.retrieve()
@@ -11,6 +12,12 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
+
+def getAccount():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
---
+++ demos/web3_py/brownie_simple_storage/scripts/read_value.py
@@ -0,0 +1,11 @@
+from brownie import SimpleStorage, accounts, config
+
+def read_contract():
+    simple_storage = SimpleStorage[-1]
+    # take the indec thats one less that the length
+    # abi
+    # address
+
+def main():
+    read_contract()
+    
