7ac47786d0caea6ea6c9c9759844d5f563c53aa5
Andrew Pianta <a.pianta92@gmail.com>
2022-03-10 13:28:10 -0500

    adds deploy_mocks script in helpful_scripts.py, implement in deploy.py

--- demos/web3_py/brownie_fund_me/fund_me/scripts/deploy.py
+++ demos/web3_py/brownie_fund_me/fund_me/scripts/deploy.py
@@ -1,5 +1,6 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, deploy_mocks
+from web3 import Web3
 
 
 def deploy_fund_me():
@@ -12,16 +13,10 @@ def deploy_fund_me():
             "eth_usd_price_feed"
         ]
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed!")
-
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
     fund_me = FundMe.deploy(
-        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        price_feed_address,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
