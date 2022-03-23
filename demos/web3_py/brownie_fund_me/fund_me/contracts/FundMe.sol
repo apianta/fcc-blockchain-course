8b2abf44a3d3cb8ac3d67befd75069c3fc1f2206
Andrew Pianta <a.pianta92@gmail.com>
2022-03-23 13:47:34 -0400

    FundMe.sol formatting

--- demos/web3_py/brownie_fund_me/fund_me/contracts/FundMe.sol
+++ demos/web3_py/brownie_fund_me/fund_me/contracts/FundMe.sol
@@ -33,7 +33,6 @@ contract FundMe {
     }
 
     function getPrice() public view returns (uint256) {
-
         (, int256 answer, , , ) = priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
     }
