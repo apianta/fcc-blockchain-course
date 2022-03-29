d76a906232175973e42939c56dd696df2ba193cb
Andrew Pianta <a.pianta92@gmail.com>
2022-03-29 15:44:21 -0400

    adds mockv3aggregator to tests folder

--- demos/web3_py/brownie_fund_me/fund_me/scripts/deploy.py
+++ demos/web3_py/brownie_fund_me/fund_me/scripts/deploy.py
@@ -1,6 +1,9 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account, deploy_mocks
-from web3 import Web3
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
