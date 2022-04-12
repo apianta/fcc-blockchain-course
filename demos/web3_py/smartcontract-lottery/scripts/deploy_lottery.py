6ae5fd5720708ea5af458e53d6900793d9e4c0ed
Andrew Pianta <a.pianta92@gmail.com>
2022-04-12 17:15:47 -0400

    start integrations test

--- demos/web3_py/smartcontract-lottery/scripts/deploy_lottery.py
+++ demos/web3_py/smartcontract-lottery/scripts/deploy_lottery.py
@@ -5,7 +5,7 @@ import time
 
 def deploy_lottery():
     account = get_account()
-    Lottery.deploy(
+    lottery = Lottery.deploy(
         get_contract("eth_usd_price_feed").address,
         get_contract("vrf_coordinator").address,
         get_contract("link_token").address,
@@ -15,7 +15,7 @@ def deploy_lottery():
         publish_source=config["networks"][network.show_active()].get("verify", False),
     )
     print("Deployed Lottery!")
-    return Lottery
+    return lottery
 
 
 def start_lottery():
