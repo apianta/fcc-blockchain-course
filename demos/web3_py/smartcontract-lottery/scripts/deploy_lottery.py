ed964fee3aa303efdbc525f03b32707200b07a54
Andrew Pianta <a.pianta92@gmail.com>
2022-04-12 11:20:05 -0400

    updates deploy script and adds new interface contract

--- demos/web3_py/smartcontract-lottery/scripts/deploy_lottery.py
+++ demos/web3_py/smartcontract-lottery/scripts/deploy_lottery.py
@@ -1,5 +1,6 @@
-from scripts.helpful_scripts import get_account, get_contract
+from scripts.helpful_scripts import get_account, get_contract, fund_with_link
 from brownie import Lottery, network, config
+import time
 
 
 def deploy_lottery():
@@ -17,5 +18,39 @@ def deploy_lottery():
     return Lottery
 
 
+def start_lottery():
+    account = get_account()
+    lottery = lottery[-1]
+    lottery.startLottery({"from": account})
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("The lottery is started!")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # fund the contract
+    # then end the lottery
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_transaction = lottery.endLottery({"from": account})
+    ending_transaction.wait(1)
+    time.sleep(60)
+    print(f"{lottery.recentWinner()} is the new winner!")
+
+
 def main():
     deploy_lottery()
+    enter_lottery()
+    start_lottery()
+    end_lottery()
