ea8abda430c72acde5e43c7c96d6d572164e6df1
Andrew Pianta <a.pianta92@gmail.com>
2022-04-19 15:14:44 -0400

    Add interface contracts.

--- demos/brownie/aave-brownie-py/scripts/aave_borrow.py
+++ demos/brownie/aave-brownie-py/scripts/aave_borrow.py
@@ -1,4 +1,4 @@
-from brownie import network, config
+from brownie import network, config, interface
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
 
@@ -6,12 +6,17 @@ from scripts.get_weth import get_weth
 def main():
     account = get_account()
     erc20_addrss = config["networks"][network.show_active()]["weth_token"]
-        # Address:
+    # Address:
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
         # ABI:
     lending_pool = get_lending_pool()
 
+
 def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
     # ABI:
-    # address:
+    
