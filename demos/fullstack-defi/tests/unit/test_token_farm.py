88b0253277bfe65cf509d416cd38dc5fae382f39
Andrew Pianta <a.pianta92@gmail.com>
2022-05-18 18:09:08 -0400

    Finish and run test_stake_tokens

--- demos/fullstack-defi/tests/unit/test_token_farm.py
+++ demos/fullstack-defi/tests/unit/test_token_farm.py
@@ -28,3 +28,24 @@ def test_set_price_feed_contract():
         token_farm.setPriceFeedContract(
             dapp_token.address, price_feed_address, {"from": non_owner}
         )
+
+
+def test_stake_tokens(amount_staked):
+    # arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+    # assert
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
+    assert token_farm.stakers(0) == account.address
+    return token_farm, dapp_token
+
+
+def test_issue_tokens():
+    
