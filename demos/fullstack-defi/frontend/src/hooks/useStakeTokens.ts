771a7a923c280063447b439135bbc20a8bbca4a4
Andrew Pianta <a.pianta92@gmail.com>
2022-05-25 18:49:32 -0400

    Move useStakeTokens

---
+++ demos/fullstack-defi/frontend/src/hooks/useStakeTokens.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import { useContractFunction, useEthers } from "@usedapp/core"
+import TokenFarm from "../chain-info/contracts/TokenFarm.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import { Contract } from "@ethersproject/contracts"
+import ERC20 from "../chain-info/contracts/MockERC20.json"
+import { constants, utils } from "ethers"
+
+export const useStakeTokens = (tokenAddress: string) => {
+	// address
+	// abi
+	// chainId
+	const { chainId } = useEthers()
+	const { abi } = TokenFarm
+	const tokenFarmAddress = chainId
+		? networkMapping[String(chainId)]["TokenFarm"][0]
+		: constants.AddressZero
+	const tokenFarmInterface = new utils.Interface(abi)
+	const tokenFarmContract = new Contract(tokenFarmAddress, tokenFarmInterface)
+
+	const erc20ABI = ERC20.abi
+	const erc20Interface = new utils.Interface(erc20ABI)
+	const erc20Contract = new Contract(tokenAddress, erc20Interface)
+	// approve
+	const { send: approveErc20Send, state: approvedErc20State } =
+		useContractFunction(erc20Contract, "approve", {
+			transactionName: "Approve ERC20 transfer",
+		})
+	const approveAndStake = (amount: string) => {
+		setAmountToStake(amount)
+		return approveErc20Send(tokenFarmAddress, amount)
+	}
+	// stake
+	const { send: stakeSend, state: stakeState } = useContractFunction(
+		tokenFarmContract,
+		"stakeTokens",
+		{ transactionName: "Stake Tokens" }
+	)
+	const [amountToStake, setAmountToStake] = useState("0")
+	// useEffect
+	useEffect(() => {
+		if (approvedErc20State.status === "Success") {
+			stakeSend(amountToStake, tokenAddress)
+		}
+	}, [approvedErc20State])
+
+	return { approveAndStake, approvedErc20State }
+}
