4c9463a98355d7a22efbcca31d6d15c9df656957
Andrew Pianta <a.pianta92@gmail.com>
2022-05-25 17:13:15 -0400

    Start useStakeToken hook

--- demos/fullstack-defi/frontend/src/components/yourWallet/YourWallet.tsx
+++ demos/fullstack-defi/frontend/src/components/yourWallet/YourWallet.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react"
 import { Box } from "@material-ui/core"
 import { TabContext, TabList, TabPanel } from "@mui/lab"
 import { Tab } from "@material-ui/core"
+import { WalletBalance } from "./WalletBalance"
+import { StakeForm } from "./StakeForm"
 
 interface YourWalletProps {
     supportedTokens: Array<Token>
@@ -10,21 +12,36 @@ interface YourWalletProps {
 
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
     const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0)
-
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setSelectedTokenIndex(parseInt(newValue))
+    }
+    // const classes = useStyles()
     return (
         <Box>
-            <h1>Your Wallet</h1>
+            <h1> Your Wallet! </h1>
             <Box>
                 <TabContext value={selectedTokenIndex.toString()}>
-                    <TabList aria-label="stake form tabs">
-                        {supportedTokens.map((token, index) =>}
-                        return (
-                        <Tab label={token.name}
-                            value={index.toString()}
-                            key={index}
-                        ))
+                    <TabList onChange={handleChange} aria-label="stake form tabs">
+                        {supportedTokens.map((token, index) => {
+                            return (
+                                <Tab label={token.name}
+                                    value={index.toString()}
+                                    key={index} />
+                            )
+                        })}
                     </TabList>
+                    {supportedTokens.map((token, index) => {
+                        return (
+                            <TabPanel value={index.toString()} key={index}>
+                                <div>
+                                    <WalletBalance token={supportedTokens[selectedTokenIndex]} />
+                                    <StakeForm token={supportedTokens[selectedTokenIndex]} />
+                                </div>
+                            </TabPanel>
+                        )
+                    })}
                 </TabContext>
             </Box>
-        </Box>
+        </Box >
+    )
 }
