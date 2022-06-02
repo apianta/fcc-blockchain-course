d7ecdc50babbbceb739fe548664cfcfde8388913
Andrew Pianta <a.pianta92@gmail.com>
2022-06-02 15:37:17 -0400

    Formatting

--- demos/fullstack-defi/frontend/src/hooks/useStakeTokens.ts
+++ demos/fullstack-defi/frontend/src/hooks/useStakeTokens.ts
@@ -1,35 +1,38 @@
-import { useEffect, useState } from "react"
-import { useEthers, useContractFunction } from "@usedapp/core"
-import { constants, utils } from "ethers"
-import TokenFarm from "../chain-info/contracts/TokenFarm.json"
-import ERC20 from "../chain-info/contracts/MockERC20.json"
-import { Contract } from "@ethersproject/contracts"
-import networkMapping from "../chain-info/deployments/map.json"
+import { useEffect, useState } from "react";
+import { useEthers, useContractFunction } from "@usedapp/core";
+import { constants, utils } from "ethers";
+import TokenFarm from "../chain-info/contracts/TokenFarm.json";
+import ERC20 from "../chain-info/contracts/MockERC20.json";
+import { Contract } from "@ethersproject/contracts";
+import networkMapping from "../chain-info/deployments/map.json";
 
 export const useStakeTokens = (tokenAddress: string) => {
 	// address
 	// abi
 	// chainId
-	const { chainId } = useEthers()
-	const { abi } = TokenFarm
+	const { chainId } = useEthers();
+	const { abi } = TokenFarm;
 	const tokenFarmAddress = chainId
 		? networkMapping[String(chainId)]["TokenFarm"][0]
-		: constants.AddressZero
-	const tokenFarmInterface = new utils.Interface(abi)
-	const tokenFarmContract = new Contract(tokenFarmAddress, tokenFarmInterface)
+		: constants.AddressZero;
+	const tokenFarmInterface = new utils.Interface(abi);
+	const tokenFarmContract = new Contract(
+		tokenFarmAddress,
+		tokenFarmInterface
+	);
 
-	const erc20ABI = ERC20.abi
-	const erc20Interface = new utils.Interface(erc20ABI)
-	const erc20Contract = new Contract(tokenAddress, erc20Interface)
+	const erc20ABI = ERC20.abi;
+	const erc20Interface = new utils.Interface(erc20ABI);
+	const erc20Contract = new Contract(tokenAddress, erc20Interface);
 	// approve
 	const { send: approveErc20Send, state: approveAndStakeErc20State } =
 		useContractFunction(erc20Contract, "approve", {
 			transactionName: "Approve ERC20 transfer",
-		})
+		});
 	const approveAndStake = (amount: string) => {
-		setAmountToStake(amount)
-		return approveErc20Send(tokenFarmAddress, amount)
-	}
+		setAmountToStake(amount);
+		return approveErc20Send(tokenFarmAddress, amount);
+	};
 	// stake
 	const { send: stakeSend, state: stakeState } = useContractFunction(
 		tokenFarmContract,
@@ -37,26 +40,26 @@ export const useStakeTokens = (tokenAddress: string) => {
 		{
 			transactionName: "Stake Tokens",
 		}
-	)
-	const [amountToStake, setAmountToStake] = useState("0")
+	);
+	const [amountToStake, setAmountToStake] = useState("0");
 
 	//useEffect
 	useEffect(() => {
 		if (approveAndStakeErc20State.status === "Success") {
-			stakeSend(amountToStake, tokenAddress)
+			stakeSend(amountToStake, tokenAddress);
 		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [approveAndStakeErc20State, amountToStake, tokenAddress])
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [approveAndStakeErc20State, amountToStake, tokenAddress]);
 
-	const [state, setState] = useState(approveAndStakeErc20State)
+	const [state, setState] = useState(approveAndStakeErc20State);
 
 	useEffect(() => {
 		if (approveAndStakeErc20State.status === "Success") {
-			setState(stakeState)
+			setState(stakeState);
 		} else {
-			setState(approveAndStakeErc20State)
+			setState(approveAndStakeErc20State);
 		}
-	}, [approveAndStakeErc20State, stakeState])
+	}, [approveAndStakeErc20State, stakeState]);
 
-	return { approveAndStake, state }
-}
+	return { approveAndStake, state };
+};
