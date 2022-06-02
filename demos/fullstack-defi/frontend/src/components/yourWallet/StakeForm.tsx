687e7f11bfa696e2b5eef13effac9c60967274e0
Andrew Pianta <a.pianta92@gmail.com>
2022-06-02 15:36:18 -0400

    Formatting

--- demos/fullstack-defi/frontend/src/components/yourWallet/StakeForm.tsx
+++ demos/fullstack-defi/frontend/src/components/yourWallet/StakeForm.tsx
@@ -1,42 +1,55 @@
-import React, { useState, useEffect } from "react"
-import { Token } from "../Main"
-import { useEthers, useTokenBalance, useNotifications } from "@usedapp/core"
-import { formatUnits } from "@ethersproject/units"
-import { Button, Input, CircularProgress } from "@material-ui/core"
-import { useStakeTokens } from "../../hooks"
-import { utils } from "ethers"
+import React, { useState, useEffect } from "react";
+import { Token } from "../Main";
+import { useEthers, useTokenBalance, useNotifications } from "@usedapp/core";
+import { formatUnits } from "@ethersproject/units";
+import {
+	Button,
+	Input,
+	CircularProgress,
+	Snackbar,
+	Alert,
+} from "@mui/material";
+import { useStakeTokens } from "../../hooks";
+import { utils } from "ethers";
 
 export interface StakeFormProps {
-	token: Token
+	token: Token;
 }
 
 export const StakeForm = ({ token }: StakeFormProps) => {
-	const { address: tokenAddress, name } = token
-	const { account } = useEthers()
-	const tokenBalance = useTokenBalance(tokenAddress, account)
+	const { address: tokenAddress, name } = token;
+	const { account } = useEthers();
+	const tokenBalance = useTokenBalance(tokenAddress, account);
 	const formattedTokenBalance: number = tokenBalance
 		? parseFloat(formatUnits(tokenBalance, 18))
-		: 0
-	const { notifications } = useNotifications()
+		: 0;
+	const { notifications } = useNotifications();
 
 	const [amount, setAmount] = useState<
 		number | string | Array<number | string>
-	>(0)
+	>(0);
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const newAmount =
-			event.target.value === "" ? "" : Number(event.target.value)
-		setAmount(newAmount)
-		console.log(newAmount)
-	}
+			event.target.value === "" ? "" : Number(event.target.value);
+		setAmount(newAmount);
+		console.log(newAmount);
+	};
 
 	const { approveAndStake, state: approveAndStakeErc20State } =
-		useStakeTokens(tokenAddress)
+		useStakeTokens(tokenAddress);
 	const handleStakeSubmit = () => {
-		const amountAsWei = utils.parseEther(amount.toString())
-		return approveAndStake(amountAsWei.toString())
-	}
+		const amountAsWei = utils.parseEther(amount.toString());
+		return approveAndStake(amountAsWei.toString());
+	};
 
-	const isMining = approveAndStakeErc20State.status === "Mining"
+	const isMining = approveAndStakeErc20State.status === "Mining";
+	const [showErc20ApprovalSuccess, setShowErc20ApprovalSuccess] =
+		useState(false);
+	const [showStakeTokenSuccess, setShowStakeTokenSuccess] = useState(false);
+	const handleCloseSnack = () => {
+		setShowErc20ApprovalSuccess(false);
+		setShowStakeTokenSuccess(false);
+	};
 
 	useEffect(() => {
 		if (
@@ -46,7 +59,8 @@ export const StakeForm = ({ token }: StakeFormProps) => {
 					notification.transactionName === "Approve ERC20 transfer"
 			).length > 0
 		) {
-			console.log("Approved!")
+			setShowErc20ApprovalSuccess(true);
+			setShowStakeTokenSuccess(false);
 		}
 		if (
 			notifications.filter(
@@ -55,21 +69,43 @@ export const StakeForm = ({ token }: StakeFormProps) => {
 					notifications.transactionName === "Stake Tokens"
 			).length > 0
 		) {
-			console.log("Tokens Staked!")
+			setShowErc20ApprovalSuccess(false);
+			setShowStakeTokenSuccess(true);
 		}
-	}, [notifications])
+	}, [notifications, showErc20ApprovalSuccess, showStakeTokenSuccess]);
 
 	return (
 		<>
-			<Input onChange={handleInputChange} />
-			<Button
-				onClick={handleStakeSubmit}
-				color="primary"
-				size="large"
-				disabled={isMining}
+			<div>
+				<Input onChange={handleInputChange} />
+				<Button
+					onClick={handleStakeSubmit}
+					color="primary"
+					size="large"
+					disabled={isMining}
+				>
+					{isMining ? <CircularProgress size={26} /> : "Stake"}
+				</Button>
+			</div>
+			<Snackbar
+				open={showErc20ApprovalSuccess}
+				autoHideDuration={5000}
+				onClose={handleCloseSnack}
 			>
-				{isMining ? <CircularProgress size={26} /> : "Stake"}
-			</Button>
+				<Alert onClose={handleCloseSnack} severity="success">
+					ERC-20 token transder approved! Now, approve the second
+					transaction.
+				</Alert>
+			</Snackbar>
+			<Snackbar
+				open={showStakeTokenSuccess}
+				autoHideDuration={5000}
+				onClose={handleCloseSnack}
+			>
+				<Alert onClose={handleCloseSnack} severity="success">
+					Tokens Staked!
+				</Alert>
+			</Snackbar>
 		</>
-	)
-}
+	);
+};
